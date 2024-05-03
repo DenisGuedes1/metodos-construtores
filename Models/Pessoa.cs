187babe6bac12592bddfe3b402d13253420366d6
DenisGuedes1 <denisguedesmoraes@gmail.com>
2024-05-03 11:51:41 -0300

    aula sobre metodos e construtores

---
+++ Models/Pessoa.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace ExemplosExplorando.Models
+{
+    public class Pessoa
+    {
+        //construtor precisa ter o mesmo nome da classe e nao possui tipo de retorno
+        public Pessoa(string nome, string sobrenome)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            
+        }
+        public Pessoa()        
+        {
+
+        }
+        //validações 
+        private string _nome;
+        private int _idade;
+        private string _sobrenome;
+        public string NomeCompleto => $" {_nome} { _sobrenome} ";
+        public string Nome 
+        {
+             get
+             {
+                return _nome.ToUpper();
+             } 
+             
+             set
+             {
+                if (value == ""){
+                    throw new ArgumentException("o nome nao poser ser vazio.");
+                }
+
+                _nome = value;
+             } 
+        
+        }
+
+        public string Sobrenome
+        {
+            get
+            {
+                return _sobrenome;
+            }
+            
+            set
+            {
+                if (value == ""){
+                    throw new ArgumentException("o nome nao poser ser vazio.");
+                }
+
+                _sobrenome = value;
+
+            }
+        
+        }
+
+        public int Idade 
+        { get => _idade;
+        
+         set
+         {
+            if (value < 0)
+            {
+                throw new ArgumentException("A idade nao pode ser menor que 0");
+            }
+            _idade = value;
+         }
+        
+        }
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome: {Nome},Idade: {Idade}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using ExemplosExplorando.Models;
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "gustavo";
+// p1.Idade = -1;
+// p1.Apresentar();
+
+//usando construtor.
+Pessoa p1 = new Pessoa(nome:"Leonardo", sobrenome:"Buta");
+Pessoa p2 = new Pessoa(nome:"Beatriz", sobrenome:"Gabriella");
+
+// Console.WriteLine($"Nome completo é {p1.NomeCompleto}");
+
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "iNGLES";
+
+cursoDeIngles.Alunos = new List<Pessoa>();
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
